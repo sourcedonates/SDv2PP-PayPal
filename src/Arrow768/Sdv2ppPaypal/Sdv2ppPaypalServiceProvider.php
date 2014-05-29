2921d8bd52d7468fac0b1929463fb1c87d4d0342
Werner 'Arrow768 <arrow768@sourcedonates.com>
2014-05-29 17:10:40 +0200

    Initial commit

---
+++ src/Arrow768/Sdv2ppPaypal/Sdv2ppPaypalServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Arrow768\Sdv2ppPaypal;
+
+use Illuminate\Support\ServiceProvider;
+
+class Sdv2ppPaypalServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('arrow768/sdv2pp-paypal');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
